1f628eab1056e225c2b57bf90d42b671cc7d8b1e
ElanHashem <103283907+ElanHashem@users.noreply.github.com>
2023-08-11 00:48:56 -0700

    push1

--- ArbitrageCalculator.py
+++ ArbitrageCalculator.py
@@ -1,1 +1,14 @@
-
+import csv
+print("sucsess")
+filename = "ArbitrageData.csv"
+ticker_names = ['USD',"EUR","GBP","JPY","CAD","AUD","MXN","SEK","SGD"]
+with open(filename, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    for ticker in ticker_names:
+        for comp in ticker_names:
+            if ticker != comp:
+                word = ticker+'/'+comp
+                row = [word]
+                csvwriter.writerow(row)
+print("all done")
+                
